d85615ec14219ee82c9e234db8a6971bcbf5837f
alexmortelsison <alexmortelsison@gmail.com>
2024-04-01 07:48:08 +0900

    Initial Commit

---
+++ Weather Test/Weather_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_TestApp.swift
+//  Weather Test
+//
+//  Created by Alex Sison on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
